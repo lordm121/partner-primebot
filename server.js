24171de439c9ebec65a10a47495826b0a061ba37
Glitch (glossy-shared-pressure) <none>
2021-06-05 20:30:48 +0000

    💾🌉 Checkpoint ./server.js:25372772/2676

---
+++ server.js
@@ -0,0 +1,68 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const db = require('quick.db')
+//const { Color } = require("./config.js");
+const fs = require("fs");
+const request = require("request");
+const prefix = "!";
+const { Collection, MessageEmbed } = require("discord.js");
+const beautify = require("js-beautify");
+const { inspect } = require("util");
+///let dev = ["738478465870987425","386188491953799178"];
+const cmd = require("node-cmd");
+//const { I18n } = require("locale-parser 
+//bot.reva = new I18n({ defaultLocale: "en" });
+
+global.logChannel = bot.channels.cache.get("835968578699264011")
+global.mongoose = require('mongoose')
+mongoose.connect("mongodb+srv://solin:hama1234@cluster0.prbzz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+  console.log("Connected to the Mongodb database.");
+}).catch((err) => {
+  console.log("Unable to connect to the Mongodb database. Error:" + err);
+});
+global.Guild = require("./data/guild.js");
+///global.User = require("./data/user.js");
+///global.Lang = require("./data/lang.js");
+///global.Owner = require("./data/owner.js");
+bot.commands = new Collection();
+bot.aliases = new Collection();
+bot.cooldowns = new Collection();
+bot.catagories = fs.readdirSync("./commands/");
+["command"].forEach(handler => {
+  require(`./handler/${handler}`)(bot);
+});
+
+/**/
+let util = require("util"),
+  readdir = util.promisify(fs.readdir);
+
+const init = async () => {
+  // Then we load events, which will include our message and ready event.
+  const evtFiles = await readdir("./events/");
+  console.log(`Loading a total of ${evtFiles.length} events.`, "log");
+  evtFiles.forEach(file => {
+    const eventName = file.split(".")[0];
+    console.log(`Loading Event: ${eventName}`);
+    const event = new(require(`./events/${file}`))(bot);
+    bot.on(eventName, (...args) => event.run(...args, bot));
+    delete require.cache[require.resolve(`./events/${file}`)];
+  });
+};
+init();
+
+bot.on("ready", () => {
+  console.log(`[!]-------------------------------------[!]`);
+  console.log(`Display Name : ${bot.user.username}`);
+  console.log(`Public Prefix : ${prefix}`);
+  console.log(`Version : 4.0.0`);
+  console.log(`[!]-------------------------------------[!]`);
+});
+
+bot.on("ready", () => {
+    function randomStatus() {
+        let status = [`s!help on ${bot.guilds.cache.size} Servers | v2.7.3`, `s!help | v2.7.3`]
+        let rstatus = Math.floor(Math.random() * status.length);
+        bot.user.setActivity(status[rstatus], {type: "PLAYING"});    
+    }; setInterval(randomStatus, 3000)
+})
+bot.login("");
