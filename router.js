0b6b70cbff40065e373fcd54279a4e173d352e89
Glitch (glossy-shared-pressure) <none>
2021-06-05 14:58:22 +0000

    🚣📇 Checkpoint

--- router.js
+++
@@ -1,7 +0,0 @@
-module.exports = (app) => {
-    // '/'
-    app.use('/', require('./routes/index'));
-    // '/authorize'
-    app.use('/authorize', require('./routes/discord'));
-    app.use('/manage',require('./routes/guildroutes'))
-}
