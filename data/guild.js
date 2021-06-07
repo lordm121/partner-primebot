2e4ad9d5a3718e9ea9b89bdd2f4edf27d00851dc
Glitch (glossy-shared-pressure) <none>
2021-06-07 16:44:46 +0000

    🐄💳 Checkpoint

--- data/guild.js
+++ data/guild.js
@@ -1,12 +1,12 @@
-const mongoose = require ('mongoose'),
-languages = require("../languages/language-meta.json");
+const mongoose = require ('mongoose')
+
 
 const Schema = new mongoose.Schema({
     guildID: String,
     channelID: String,
   guildName: String,
      prefix: { type: String, default: "!"},
-  language: { type: String, default: languages.find((l) => l.default).name }, // Language of the guild
+
 
 })
 
