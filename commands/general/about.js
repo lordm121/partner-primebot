b0da00c62a5c94d9c93c0831eb142a98afe70cf2
Glitch (glossy-shared-pressure) <none>
2021-06-07 08:41:55 +0000

    💪👜 Checkpoint ./commands/general/about.js:25372772/14551 ./package.json:25372772/2

---
+++ commands/general/about.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const Discord = require("discord.js");
+
+
+module.exports = {
+  name: "about.js",
+  aliases: ["bot","botinfo","bot-info"],
+  description: "information about bot",
+  usage: [".botinfo"],
+  category: ["general"],
+  enabled: true,			
+  memberPermissions: [ "SEND_MESSAGES" ],			
+  botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],		
+  ownerOnly: false,			
+  guilOwnerOnly: true,
+  cooldown: 3000,
+  run: async (bot, message, args) => {
+
+       let embed = new Discord.MessageEmbed()
+        .setTitle(`Info`,` ${bot.user.username}`,true)
+        .setColor("")
+        .setThumbnail(bot.user.displayAvatarURL())
+        .addField(`**My Name:**`, `${bot.user.tag}`)
+        .addField(`**My ID**`, `${bot.user.id}`)
+        .addField(`**My Prefix**`, `.`)
+        .addField(`**Libary**` , `discord.js`)
+        .addField(`**Discord.js Version**`, `${Discord.version}`)
+        
+        .addField(`**Ping**`, `${Math.round(bot.ws.ping)}ms`)
+        .addField(`**Guilds**`, `${bot.guilds.cache.size}`)
+        .addField(`**Channels**`, `${bot.channels.cache.size}`)
+        .addField(`**Users**`, `${bot.users.cache.size}`)
+        .addField(`**Creator**`, `[<@768944616724103170>]`)
+        .setFooter(`Requested By ${message.author.username}`)
+        
+    message.channel.send(embed);
+}
+}
