54619c966e0cf137d919e299a9d0a85ff878977e
Glitch (glossy-shared-pressure) <none>
2021-06-11 16:22:01 +0000

    🛋🙎 Checkpoint ./commands/admin/premuim.js:25372772/390

--- commands/admin/premuim.js
+++ commands/admin/premuim.js
@@ -17,19 +17,19 @@ module.exports = {
   run: async (bot, message, args, dev) => {
 if (!devs.includes(message.author.id)) return;
     if (db.get(`${message.guild.id}.serverPlan`) == 'Free') 
-      return embed.setDescription(`**ان سيرفر \`${db.get(`${message.guild.id}.serverName`)}\`  ليس مشترك في الـ \`Premium\` ⚠️**`),
+      return embed.setDescription(`**Your server dont have premuim version \`${db.get(`${message.guild.id}.serverName`)}\`ـ if you want\`Premium\` version please join support server and send messag to <@768944616724103170> ⚠️**`),
         message.channel.send(embed)
    // const cooldown = 2.4192e9;
-  const cooldown = (30 * 24 * 3600);
+  const cooldown = 2592000000;
     const preTime = db.get(`${message.guild.id}.serverPlanTime`); // الوقت بتاع المستخدم فيه كام ثانية
     const preNow = cooldown - (Date.now() - preTime);
     if (preTime !== null && preNow > 0) {
-      embed.setDescription(`** \`${pretty(preNow, { verbose: true })}\`**`), message.channel.send(embed);
+      embed.setDescription(`**premuim version time**   \`${pretty(preNow, { verbose: true })}\`**`), message.channel.send(embed);
     } else {
-      db.set(`${message.guild.id}.serverLanguage`, 'arabic')
+      db.set(`${message.guild.id}.serverLanguage`, 'english')
       db.set(`${message.guild.id}.serverPrefix`, '-')
       db.set(`${message.guild.id}.serverPlan`, 'Free')
       db.set(`${message.guild.id}.serverPlanTime`, null)
-      embed.setDescription(`**انتهي اشتراك البرايم في هذا السيرفر ⚠️**`),
+      embed.setDescription(`**Premium version ended ⚠️**`),
       message.channel.send(embed)
     }}}
