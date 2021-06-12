9915bc92b38869815fb19b7b5614fd38b562bb4a
Glitch (glossy-shared-pressure) <none>
2021-06-12 11:47:55 +0000

    🎽🌿 Checkpoint ./commands/admin/post.js:25372772/986

--- commands/admin/post.js
+++ commands/admin/post.js
@@ -94,7 +94,7 @@ if (!message.guild.member(bot.user).hasPermission("ADMINISTRATOR")) return embed
 
 function hook(messagePost, channelsPost, client) {
   try {
-    channelsPost.send({ embed: messagePost });
+    channelsPost.send(`:crown: __**Owner:**__ ${message.guild.owner ? message.guild.owner.user.tag : message.guild.author.tag}\n:earth_africa: __**Region:**__ ${message.guild.region}\n:timer: __**Created:**__ ${message.guild.createdAt.toLocaleString()}\n\n${db.get(`${message.guild.id}.serverDescription`) ? db.get(`${message.guild.id}.serverDescription`) : ''}\n\n:link: **Server Invite**\n**[Join Now](${db.get(`${message.guild.id}.serverInvite`) || invite.url})**\n:busts_in_silhouette: **Members** \`${message.guild.memberCount}\`\n**Humans:** \`${(message.guild.memberCount - message.guild.members.cache.filter(m => m.u`);
     channelsPost.createOverwrite(channelsPost.guild.id, {
       SEND_MESSAGES: false,
       READ_MESSAGES: true,
