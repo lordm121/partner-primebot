bc426e2d0c924d3fd93559926f9fdeb39d828463
Glitch (glossy-shared-pressure) <none>
2021-06-12 15:25:33 +0000

    🤑🎠 Checkpoint ./commands/admin/setchannel.js:25372772/3708

--- commands/admin/setchannel.js
+++ commands/admin/setchannel.js
@@ -14,22 +14,25 @@ module.exports = {
   memberPermissions: [ "MANAGE_CHANNELS" ],            
   botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS","MANAGE_CHANNELS"],        
   ownerOnly: false,            
-  cooldown: 10000,
+  cooldown: 10000,  cooldown: 10000,
+
   run: async (bot, message, args, dev, data) => {
+  if(db.get(`${message.guild.id}.serverPlan`) === 'premuim')return embed.setDescription(`**ان سيرفر \`${db.get(`${message.guild.id}.serverName`)}\`  ليس مشترك في الـ \`Premium\` ⚠️**`), message.channel.send(embed)
+ 
 if (!message.guild.member(message.author).hasPermission('ADMINISTRATOR')) return embed.setColor('#FF0202').setDescription(`**You don't have  \`ADMINISTRATOR\` permission | 🤔**`), message.channel.send(embed)
 
     let messageSetting = message.mentions.channels.first();
     let ch = message.guild.channels.cache.find(c => c.id == messageSetting.id);
 
-    if (!messageSetting || !ch) return embed.setColor('#FF0202').setDescription(`**برجاء قم بعمل منشن للروم/التشانل الخاصة للنشر! | ⚠️**`), message.channel.send(embed)
+    if (!messageSetting || !ch) return embed.setColor('#FF0202').setDescription(`**You must mention room to setup channel! | ⚠️**`), message.channel.send(embed)
 
     if (db.has(`${message.guild.id}.serverPostChannel`) && db.get(`${message.guild.id}.serverPostChannel`) == ch.id) {
-      embed.setColor('#FF0202').setDescription(`**لقد قمت بالفعل بإضافة تلك التشانل من قبل! | ❌**`);
+      embed.setColor('#FF0202').setDescription(`**This room Already on databse! | ❌**`);
       message.channel.send(embed)
       return;
     };
 
     db.set(`${message.guild.id}.serverPostChannel`, ch.id);
-    embed.setDescription(`**.لقد تم التثبيت بنجاح | ☑️**`), message.channel.send(embed)
+    embed.setDescription(`** This channel saved on database | ☑️**`), message.channel.send(embed)
 
   }}
