ab0f3f4b077037472746ebc656c81631342eaf7b
Glitch (glossy-shared-pressure) <none>
2021-06-12 19:28:26 +0000

    🏓🐔 Checkpoint ./commands/admin/setbanner.js:25372772/3879

--- commands/admin/setbanner.js
+++ commands/admin/setbanner.js
@@ -6,21 +6,22 @@ const db = require("quick.db")
 let embed = new Discord.MessageEmbed()
 module.exports = {
   name: "setbanner",
-  aliases: ["banner"],
-  description: "Change the prefix of the bot",
+  aliases: ["banner","setbanner"],
+  description: "set a banner to share message",
   usage: ["s!prefix [Prefix]"],
   category: ["Moderation"],
   enabled: true,            
-  memberPermissions: [ "ADMINISTRATOR" ],            
-  botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],        
+  memberPermissions: [ "ADMINISTRATOR","MANAGE_GUILD"],            
+  botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS","MANAGE_GUILD"],        
   ownerOnly: false,            
   cooldown: 10000,
   run: async (bot, message, args, dev, data) => {
-if (!message.guild.member(message.author).hasPermission('ADMINISTRATOR')) return embed.setDescription(`**لا تمتلك صلاحية \`ADMINISTRATOR\` | 🤔**`), message.channel.send(embed)
+////if (!message.guild.member(message.author).hasPermission('ADMINISTRATOR')) return embed.setDescription(`**لا تمتلك صلاحية \`ADMINISTRATOR\` | 🤔**`), message.channel.send(embed)
+if (db.get(`${message.guild.id}.serverPlan`) == 'Free') return embed.setDescription(`**This server \`${db.get(`${message.guild.name}.serverName`)}\` Don't have \`Premium\`  version⚠️**`), message.channel.send(embed)
 
     let banner = message.content.split(' ').slice(1).join(' ');
 
-    if (!banner) return embed.setColor('#FF0202').setDescription(`**برجاء قم بوضع رابط صورة البانر! | ⚠️**`), message.channel.send(embed)
+    if (!banner) return embed.setColor('#FF0202').setDescription(`** Please send URL of a banner! | ⚠️**`), message.channel.send(embed)
     
     if (db.has(`${message.guild.id}.serverBanner`) && db.get(`${message.guild.id}.serverBanner`) == banner) {
       embed.setColor('#FF0202').setDescription(`**This image Already  | ❌**`);
