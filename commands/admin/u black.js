cfe0ea40d98ee0598bbddffc5f32370e9c12d1a5
Glitch (glossy-shared-pressure) <none>
2021-06-19 08:16:54 +0000

    🌗⚡️ Checkpoint ./commands/admin/u black.js:25372772/2146

---
+++ commands/admin/u black.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js")
+const embed = new Discord.MessageEmbed()
+const db = require("quick.db")
+const devs = "768944616724103170"
+module.exports = {
+  name: "unblack",
+  aliases: ["unb"],
+  description: "unblacklist server",
+  usage: ["unblack"],
+  category: ["onwer"],
+  enabled: true,
+  memberPermissions: ["SEND_MESSAGES"],
+  botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+  ownerOnly: false,
+  cooldown: 3000,
+  run: async (bot, message, args, dev, data) => {
+    if (!devs.includes(message.author.id)) return;
+
+    var black = message.content.split(" ").slice(1).join(" ");
+
+    if (db.has(`${message.guild.id}.serverBlackList`)) return embed.setDescription(`**ان السيرفر ليس في قائمة الحظر | ⚠️**`), message.channel.send(embed)
+    db.delete(`${message.guild.id}.serverBlackList`);
+
+    embed.setDescription(`**لقد تم إزالة الحظر من السيرفر بنجاح | ☑️**`), message.channel.send(embed)
+  }}
