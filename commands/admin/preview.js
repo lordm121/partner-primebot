04cc876d74aee833dc3e9571561b4af42e27f368
Glitch (glossy-shared-pressure) <none>
2021-06-19 21:00:18 +0000

    👧🚪 Checkpoint ./commands/admin/preview.js:25372772/398

--- commands/admin/preview.js
+++ commands/admin/preview.js
@@ -1,4 +1,4 @@
-const Discord = require("disocrd.js")
+const Discord = require("discord.js")
 const db = require("quick.db")
 let embed = new Discord.MessageEmbed()
 module.exports = {
@@ -14,13 +14,17 @@ module.exports = {
     cooldown: 6000,
     run: async (client, message, args, dev) => {
   let des= db.get(`${message.guild.id}.serverDescription`)
+  let channel = db.get(`${message.guild.id}.serverPostChannel`)
+  let color = db.get(`${message.guild.id}.serverColor`)
            embed .setTitle(` Preview [${message.guild.name}]`)
            embed .setColor("")
  
-        .setDescription(`\n **Description:**\n ${des} 
-    \n **Invite: [klick]** 
-    \n :globe_with_meridians: ${message.guild.region}
-    \n ${message.guild.memberCount}
+        .setDescription(`
+\`Description\`: \n **${des} **
+\`share channel\`:**<#${channel}>**
+\`embed Color\` : **${color}**
+:globe_with_meridians: ${message.guild.region}
+${message.guild.memberCount}
     `)
         return message.channel.send(embed)
     }}
