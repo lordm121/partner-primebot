312b9db0a275a3a029179bc7ba33b924842882af
Glitch (glossy-shared-pressure) <none>
2021-06-20 08:59:28 +0000

    🏣🦀 Checkpoint ./commands/admin/add-prime.js:25372772/478

---
+++ commands/admin/add-prime.js
@@ -0,0 +1,16 @@
+
+const Discord = require("discord.js")
+
+module.exports = {
+  name: "help",
+  aliases: ["commands"],
+  description: "To show you all command of the bot",
+  usage: ["s!help","s!help <command>"],
+  category: ["General"],
+  enabled: true,            
+  memberPermissions: [ "SEND_MESSAGES" ],            
+  botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],        
+  ownerOnly: false,            
+  cooldown: 1000,
+  run: async (bot, message, args, dev, data) => {
+   
