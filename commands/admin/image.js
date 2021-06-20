05c27e8159b0ff00cff682eb9d5d7d71bab1de35
Glitch (glossy-shared-pressure) <none>
2021-06-20 09:10:54 +0000

    🌷🖇 Checkpoint ./commands/admin/add-prime.js:25372772/30 ./commands/admin/image.js:25372772/1685

--- commands/admin/image.js
+++ commands/admin/image.js
@@ -1,9 +1,7 @@
-
 const Discord = require("discord.js")
-
 module.exports = {
-  name: "say",
-  aliases: ["say"],
+  name: "image",
+  aliases: ["iamge"],
   description: "To show you all command of the bot",
   usage: ["s!help","s!help <command>"],
   category: ["General"],
@@ -13,7 +11,6 @@ module.exports = {
   ownerOnly: false,            
   cooldown: 1000,
   run: async (bot, message, args, dev, data) => {
-  const sayMessage = args.join(" ") //a mensagem no caso tudo depois que vier do prefixo seguido do comando t.say bla bla bla (tudo que vier apأ�s o t.say serأ� escrito)
-   message.delete()//deleta a mensagem logo apأ�s executar o comando e pegar os args
- message.channel.send(sayMessage) //aqui o comando vai ser mandado no chat que foi solicitado o comando
-  }} 
+  const image = args.join("")
+  message.channel.send(`https://cdn.discordapp.com/attachments/805417634102640690/856098452669071380/Screenshot_20210620_120145_com.discord.jpg`)
+  }}
