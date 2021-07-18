f5e03c5333717e6950f14950ba900659f78c6fa6
Glitch (glossy-shared-pressure) <none>
2021-07-18 21:25:42 +0000

    👁🐿 Checkpoint ./commands/admin/share.js:25372772/450

--- commands/admin/share.js
+++ commands/admin/share.js
@@ -47,6 +47,7 @@ const { Color } = require("../../config.js");
 const db = require("quick.db")
 const pretty = require("pretty-ms");
 let embed = new Discord.MessageEmbed()
+const cdtime = 4
 module.exports = {
   name: "share",
   aliases: ["share"],
@@ -81,12 +82,19 @@ module.exports = {
     if (postChannel && !filter) return data.delete, embed.setDescription(`**If You Delete Share channel Your server will be blacklist | ⚠️**`).setColor("#FF0202"), message.channel.send(embed);
 
  // if (cooldown) && postTime !== null && cooldown *
-    
+    /*
 if(cooldown - (Date.now()) > 0 ){
       const postServerTime = cooldown - (Date.now()); // حساب الثواني المتبقية
       embed.setDescription(`**:stopwatch: | ${message.author.username}, You must wating for \n\`${pretty(postServerTime, { verbose: true })}.\` to share again**`);
       message.channel.send(embed);
-      return;
+      return;*/
+    if (cooldown.has(message.author.id)) {
+      return message.reply("wait for 24h ");
+    
+    cooldown.add(message.author.id);
+    setTimeout(() => {
+      cooldown.delete(message.author.id);
+    }, cdtime * 5000)
     } else {
       if(data) { Guild.create({
         time: cooldown,
@@ -106,13 +114,7 @@ let data = await Guild.find()
    guildID: res.guildID,
    Channel: res.Channel
  })
-        if (cooldown.has(message.guild.id)) {
-      return message.reply(`${cooldown}`);
-    }
-    cooldown.add(message.guild.id);
-    setTimeout(() => {
-      cooldown.delete(message.guild.id);
-    }
+    
         const channelsPost = bot.channels.cache.find(ch => ch.id == res.Channel)////db.get(`${res.ID}.serverPostChannel`));
         if (channelsPost) {
           const chann = bot.channels.cache.find(ch => ch.id == res.Channel)////db.get(`${message.guild.id}.serverPostChannel`));
