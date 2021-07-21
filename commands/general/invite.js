bbc47fdab12b68c4824f736a832c47ae3e0b4bbf
lordm121 <74502300+lordm121@users.noreply.github.com>
2021-07-21 23:55:08 +0300

    Update invite.js

--- commands/general/invite.js
+++ commands/general/invite.js
@@ -17,7 +17,7 @@ module.exports = {
     
 const embed = new Discord.MessageEmbed()
  .setColor(Color)
- .setTitle("Invite Link")
+ .setTitle(bot.pro.get(data.lang,"general","invite_link"))
  .setDescription(`[Partner Bot](https://discord.com/api/oauth2/authorize?client_id=838593240328044554&permissions=67206193&scope=bot)`)
  .setFooter(`by : ${message.author.tag}`)
 message.channel.send(embed);
