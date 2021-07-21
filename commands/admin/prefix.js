4c4bac41e6ea53ce1253a205a7dfd525700bef54
lordm121 <74502300+lordm121@users.noreply.github.com>
2021-07-21 23:32:11 +0300

    Update prefix.js

--- commands/admin/prefix.js
+++ commands/admin/prefix.js
@@ -16,13 +16,13 @@ module.exports = {
   prime: false,
   run: async (bot, message, args, dev, data) => {
         if(!args[1]) return message.channel.send(new Discord.MessageEmbed().setColor("").setDescription(`You muste put provide prefix`))
-        if(args[1].length > 5) return message.channel.send(new Discord.MessageEmbed().setColor("").setDescription(`please your prfix less than 5word`))
+        if(args[1].length > 5) return message.channel.send(new Discord.MessageEmbed().setColor("").setDescription(bot.pro.get(data.lang,"admin","err_prefix")))
          
         let dataa = await Guild.findOne({ guildID: message.guild.id })
 
         let embed = new Discord.MessageEmbed()
         .setColor("")
-        .setDescription(`prefix has benn changed to ${args[1]}`)
+        .setDescription(bot.pro.get(data.lang,"admin","sec_prefix"))
         message.channel.send(embed)
         dataa.prefix = args[1];
         dataa.save();
