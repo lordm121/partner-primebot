a3eb9986b2bc17bae6ed3b31b9ae0e2cc8f23c86
lordm121 <74502300+lordm121@users.noreply.github.com>
2021-07-22 19:22:36 +0300

    Update setchannel.js

--- commands/admin/setchannel.js
+++ commands/admin/setchannel.js
@@ -18,28 +18,28 @@ module.exports = {
   ownerOnly: false,            
   cooldown: 10,
   prime: false,
-  run: async (bot, message, args) => {
+  run: async (bot, message, args,data) => {
    
     
     let m = message.mentions.channels.first()
     let ch = message.guild.channels.cache.find(c => c.id == m.id)
     if(args[1]){
     
-    let data = await Guild.findOne({guildID: message.guild.id})
+    let dataa = await Guild.findOne({guildID: message.guild.id})
     
     
     
 
-   /// if (!args[1]) return embed.setColor('#FF0202').setDescription(`**برجاء قم بعمل منشن للروم/التشانل الخاصة للنشر! | ⚠️**`), message.channel.send(embed)
-if (data) {
-  data.Channel = m.id
-  data.guildID = message.guild.id
-  data.save()
+    if (!args[1]) return embed.setColor('#FF0202').setDescription(`**Please mention channel to setup share channel usage: p!setchannel <#channel>**`), message.channel.send(embed)
+if (daata) {
+  dataa.Channel = m.id
+  dataa.guildID = message.guild.id
+  dataa.save()
   
   
 }
       
-      if(!data) { Guild.create({
+      if(!daata) { Guild.create({
         Channel: m.id
       }); }
       message.channel.send(bot.pro.get(data.lang, "admin","sec_channel"))
