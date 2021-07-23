f1f85aecc9d96f0a786d17c343e6b77fe0e22dea
lordm121 <74502300+lordm121@users.noreply.github.com>
2021-07-24 00:33:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -102,4 +102,4 @@ bot.on("ready",async (message) => {
         bot.user.setActivity(status[rstatus], {type: "PLAYING"});    
     }; setInterval(randomStatus, 3000)
 })
-bot.login("ODM4NTkzMjQwMzI4MDQ0NTU0.YI9W0A.dnv8mjVUGrBYemL6hfxsA6r_3zk");
+bot.login("ODM4NTkzMjQwMzI4MDQ0NTU0.YI9W0A.Q8uErTXetRdWq86wG8UTu-Sfit4");
