d197791e671780cea19168bdc5dbb63fa7e3019f
lordm121 <74502300+lordm121@users.noreply.github.com>
2021-07-24 00:49:04 +0300

    Update server.js

--- commands/owner/server.js
+++ commands/owner/server.js
@@ -12,7 +12,127 @@ module.exports = {
   guilOwnerOnly: true,
   cooldown:0 ,
   run: async (bot, message, args, dev) => {
+    
+    if (message.author.id == ownerid) {
+      if (!message.guild.me.hasPermission("ADMINISTRATOR"))
+        return message.channel
+          .send("I Dont Have Permissions")
+          .then(msg => msg.delete({ timeout: 5000 }));
 
+      let i0 = 0;
+      let i1 = 10;
+      let page = 1;
+const owner = message.guild.owner.id
+      let description =
+        `Total Servers - ${bot.guilds.cache.size}\n\n` +
+        bot.guilds.cache
+          .sort((a, b) => b.memberCount - a.memberCount)
+          .map(r => r)
+          .map((r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members\nID - ${r.id}. guild owner = <@${owner}>`)
+          .slice(0, 10)
+          .join("\n\n");
+
+      let embed = new Discord.MessageEmbed()
+        .setAuthor(bot.user.tag, bot.user.displayAvatarURL({dynamic : true}))
+        
+        .setColor("00FFFF")
+        .setFooter(`Page - ${page}/${Math.ceil(bot.guilds.cache.size / 10)}`)
+        .setDescription(description);
+
+      let msg = await message.channel.send(embed);
+
+      await msg.react("⬅");
+      await msg.react("➡");
+      await msg.react("❌");
+
+      let collector = msg.createReactionCollector(
+        (reaction, user) => user.id === message.author.id
+      );
+
+      collector.on("collect", async (reaction, user) => {
+        if (reaction._emoji.name === "⬅") {
+          // Updates variables
+          i0 = i0 - 10;
+          i1 = i1 - 10;
+          page = page - 1;
+
+          // if there is no guild to display, delete the message
+          if (i0 + 1 < 0) {
+            console.log(i0)
+            return msg.delete();
+          }
+          if (!i0 || !i1) {
+            return msg.delete();
+          }
+
+          description =
+            `Total Servers - ${bot.guilds.cache.size}\n\n` +
+            bot.guilds.cache
+              .sort((a, b) => b.memberCount - a.memberCount)
+              .map(r => r)
+              .map(
+                (r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members\nID - ${r.id} owner = <@{owner}>`)
+              .slice(i0, i1)
+              .join("\n\n");
+
+          // Update the embed with new informations
+          embed
+            .setFooter(
+              `Page - ${page}/${Math.round(bot.guilds.cache.size / 10 + 1)}`
+            )
+            .setDescription(description);
+
+          // Edit the message
+          msg.edit(embed);
+        }
+
+        if (reaction._emoji.name === "➡") {
+          // Updates variables
+          i0 = i0 + 10;
+          i1 = i1 + 10;
+          page = page + 1;
+
+          // if there is no guild to display, delete the message
+          if (i1 > bot.guilds.cache.size + 10) {
+            return msg.delete();
+          }
+          if (!i0 || !i1) {
+            return msg.delete();
+          }
+
+          description =
+            `Total Servers - ${bot.guilds.cache.size}\n\n` +
+            bot.guilds.cache
+              .sort((a, b) => b.memberCount - a.memberCount)
+              .map(r => r)
+              .map(
+                (r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members\nID - ${r.id}`)
+              .slice(i0, i1)
+              .join("\n\n");
+
+          // Update the embed with new informations
+          embed
+            .setFooter(
+              `Page - ${page}/${Math.round(bot.guilds.cache.size / 10 + 1)}`
+            )
+            .setDescription(description);
+
+          // Edit the message
+          msg.edit(embed);
+        }
+
+        if (reaction._emoji.name === "❌") {
+          return msg.delete();
+        }
+
+        // Remove the reaction when the user react to the message
+        await reaction.users.remove(message.author.id);
+      });
+    } else {
+      return;
+    }
+    
+/*
   
     if (message.author.id == ownerid) {
       if (!message.guild.me.hasPermission("ADMINISTRATOR"))
@@ -23,14 +143,15 @@ module.exports = {
       let i0 = 0;
       let i1 = 10;
       let page = 1;
+const owner = message.guild.owner.name
 
       let description =
         `Total Servers - ${bot.guilds.cache.size}\n\n` +
         bot.guilds.cache
         /// `server owner - ${owner}`
-          .sort((a, b) => b.memberCount - a.memberCount )
+          .sort((a, b) => b.memberCount - a.memberCount - b.owner.id)
           .map(r => r)
-          .map((r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members\nID - ${r.id} `)
+          .map((r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members\nID - ${r.id} - <@${r.owner.id}> `)
           .slice(0, 10)
           .join("\n\n");
       
@@ -71,10 +192,10 @@ module.exports = {
           description =
             `Total Servers - ${bot.guilds.cache.size}\n\n` +
             bot.guilds.cache
-              .sort((a, b) => b.memberCount - a.memberCount)
+              .sort((a, b) => b.owner.id - a.memberCount)
               .map(r => r)
               .map(
-                (r, i) => `**${i + 1}** - ${r.name} |Members\nID - ${r.id} | `)
+                (r, i) => `**${i + 1}** - ${r.name} | ${r.owner.id} Members\nID - ${r.id} | ${r.owners.id} - ${r.id}`)
               .slice(i0, i1)
               .join("\n\n");
 
@@ -133,6 +254,6 @@ module.exports = {
       });
     } else {
       return;
-    }
+    }*/
   }
   }
