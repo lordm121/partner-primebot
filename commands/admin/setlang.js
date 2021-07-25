92c362243d7d5a82bf25515dc575f6cc5d14723d
lordm121 <74502300+lordm121@users.noreply.github.com>
2021-07-25 20:18:59 +0300

    Update setlang.js

--- commands/admin/setlang.js
+++ commands/admin/setlang.js
@@ -19,16 +19,16 @@ module.exports = {
   if (!args[1])
         return message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(``));
       let data = await Lang.findOne({ guildID: message.guild.id })
-      if (args[1].toLowerCase() === "english" || args[1].toLowerCase() === "kurdish" || args[1].toLowerCase() === "arabic" || args[1].toLowerCase() === "spain" || args[1].toLowerCase() === "germany") {
+      if (args[1].toLowerCase() === "english" || args[1].toLowerCase() === "kurdish" || args[1].toLowerCase() === "arabic" || args[1].toLowerCase() === "french" || args[1].toLowerCase() === "germany") {
         data.language = args[1].toLowerCase();
         message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`
           language your server has been changed to **${data.language}**`
         ));
       data.save();
       } else if (args[1] === "list") {
-        message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`Language list is :\n **english** ,**kurdish** ,**arabic** ,**spain** ,**germany**`));
+        message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`Language list is :\n **english** ,**kurdish** ,**arabic** ,**french**`));
       } else {
         message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`
-          Please Type\n \`.lang english\` \n \`.lang kurdish\` \n \`.lang arabic\` \n \`.lang spain\` \n \`.lang germany\``
+          Please Type\n \`p!lang english\` \n \`p!lang kurdish\` \n \`p!lang arabic\` \n \`p!lang french\``
         ));
      }}}
