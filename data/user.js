68b76019146cf29cfda4ae27d8b4c0d372a4d0b4
lordm121 <74502300+lordm121@users.noreply.github.com>
2021-08-16 02:07:17 +0300

    Update user.js

--- data/user.js
+++ data/user.js
@@ -1,12 +1,13 @@
 const schema = mongoose.Schema({
-    guildID: String,
-    userID: String,
-  
-    items: { type: Array, required: false, default: [] },
-
+   id: String,
+   userID: String,
+  ///items
+ items: { type: Array, required: false, default: [] },
+/////blance
 money: { type: Number, default: 0 },
-
-   time: { type: Number, default: 0 }
-
+////dailytime
+time: { type: Number, default: 0 },
+        date: Date,
+        guild: String 
 });
 module.exports = mongoose.model("User", schema)
