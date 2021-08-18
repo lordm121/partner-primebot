6ce36d99633672a3725d3e2d63ef412d65923458
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-08-18 05:12:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ init();
 bot.on("ready", async () => {
   console.log(`bot now is ready!`);
   await bot.user.setStatus("idle");
-  await bot.user.setActivity(`${prefix}help`, { type: "COMPETING" });
+  await bot.user.setActivity(`${prefix}help || www.partner-bot.tk`, { type: "COMPETING" });
  
  });
 
