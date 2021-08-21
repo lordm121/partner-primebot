478b4944b3b89f7cdd87a3502ed2e5ad85b7ed5a
lordm121 <74502300+lordm121@users.noreply.github.com>
2021-08-21 12:43:04 +0300

    Update balance.js

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -1,29 +1,26 @@
 const fs = require("fs");
 const Discord = require("discord.js");
-const { Color } = require("../../config.js");
-
 module.exports = {
   name: "balance",
-  aliases: ["balance","credit","b","credits"],
-  description: "To show your balance",
-  usage: ["p! balance"],
-  category: ["General"],
+  aliases: ["balance","credit","credits"],
+  description: "To now your credits",
+  usage: ["credit", "credit @user"], 
   enabled: true,			
   memberPermissions: [ "SEND_MESSAGES" ],			
   botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],		
   ownerOnly: false,			
-  cooldown: 15,
-  run: async (bot, message, args) => {
-    
-    let member = message.mentions.users.first() || message.author;
+  cooldown: 6000,
+  run: async (client, message, args, dev,dev2) => {
+  if (args[2]) return;
+    let member = message.guild.member(message.mentions.users.first())
+    if (member) {
+      
+  let autho = await User.findOne({ userID: member.id });    
+  message.channel.send("<@"+member.id+"> Your credits balance is `"+autho.money+"`");
   
-  
-let author = await User.findOne({ userID: member.id });    
- message.channel.send(`💰${member.username} currently have \`$${author.money}\``)
-/* message.channel.send(new Discord.MessageEmbed()
-         .setTitle(`:bank: ${user.username}\'s Balance`)
-         .setColor("BLUE")
-         .addField('Cash', `You Currently have \$${author.money} in Cash`));
-      */
-}
-}
+    }
+    if (!member) {
+  let author = await User.findOne({ userID: message.author.id });    
+  message.reply(" Your credits balance is `"+author.money+"`");
+      }
+  }};
