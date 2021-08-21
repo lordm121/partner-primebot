fbd5d39b4d44d7254f676dedf6517a98e975d06e
lordm121 <74502300+lordm121@users.noreply.github.com>
2021-08-21 11:31:04 +0300

    Update sendcredit.js

--- commands/economy/sendcredit.js
+++ commands/economy/sendcredit.js
@@ -18,7 +18,7 @@ module.exports = {
     if(args[2] < 1) return message.channel.send(`You can't send 0 credit!`)
 
     let author = await User.findOne({ userID: message.author.id });
-    let loc = await User.findOne({ guildID: message.guild.id, userID: member.id });
+    let loc = await User.findOne({ userID: member.id });
     if(!loc) return bot.nodb(member.user)
             
     if(author.money < args[2]) return message.channel.send(`You don't have this amount credit!`)
