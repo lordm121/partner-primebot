7453191c0ecc561379a4088bbdc9ea4b792f2795
lordm121 <74502300+lordm121@users.noreply.github.com>
2021-08-30 17:22:02 +0300

    Create messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -14,11 +14,11 @@ async run(message,bot) {
   let guild = await Guild.findOne({ guildID: message.guild.id});
   if(!guild) { Guild.create({ guildID: message.guild.id }); }
   data.guild = guild;
-  let lang = await Lang.findOne({ guildID: message.guild.id });
+ /* let lang = await Lang.findOne({ guildID: message.guild.id });
   if(!lang) { Lang.create({ guildID: message.guild.id });} 
-  data.lang = lang.language
+  data.lang = lang.language*/
   let prime = await Prime.findOne({ guildID: message.guild.id });
-/* if (prime && prime.log === "enable") return message.channel.send({content:`you don't have Premium version`});*/
+/*if (prime && prime.log === "enable") return message.channel.send({content:`you don't have Premium version`});*/
 let user = await User.findOne({userID: message.author.id });
   if(!user) { User.create({userID: message.author.id})}
    data.user = user
