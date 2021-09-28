7f3170407a2e271e24c3233a86813da889331afd
lordm121 <74502300+lordm121@users.noreply.github.com>
2021-09-28 16:53:25 +0300

    Delete user.js

--- data/servers/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-module.exports = mongoose.model("user-vote-servers", 
-	new mongoose.Schema({
-		id: String,
-        date: Date,
-        guild: String
-	})
-);
